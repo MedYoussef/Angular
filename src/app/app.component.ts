8ca4821baa2a957d29727b4108ca292c1fe237c8
mac <mac@MacBook-Pro.local>
2020-04-02 09:16:53 +0200

    component and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  display : boolean = false;
   username : string = 'username';
   title = 'Demo';
+  compteur = 0;
+  tableau  = new Array();
+  constructor(){}
+  ngOnInit(){
+  }
+  ClickEvent(){
+    this.display = !this.display;
+    console.log(this.compteur++);
+    this.tableau.push(this.compteur);
+  }
 }
